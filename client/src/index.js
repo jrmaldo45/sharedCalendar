354f8d8ad4e46ee0ce1c1e0110df01e3947b33ce
Jared Maldonado <jrmaldo1228@gmail.com>
2022-07-12 09:23:34 -0500

    Updated import statement to correct syntax for react 18.2.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import App from './components/App.jsx';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
+// ReactDOM.render(<App />, document.getElementById('app'));
